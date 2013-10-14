b2a5e63e351c2c04b43be938201465fd6456310d
Phitherek_ <phitherek@gmail.com>
2013-10-15 01:15:00 +0200

    Added Doxyfile, generated documentation. closes #15

---
+++ C++/SimpleLion.h
@@ -0,0 +1,21 @@
+#ifndef _SIMPLELION_H
+#define _SIMPLELION_H
+#include "LocalizationEntry.h"
+#include "LocalizationString.h"
+#include "LocalizationCategory.h"
+#include "Localization.h"
+#include "Exceptions.h"
+/// \file SimpleLion.h
+/// \brief A part of the SimpleLion project - C++ module. A common header that includes whole C++ module.
+/// \author Phitherek_
+/// \date 2013
+/// \version 0.1
+
+/*! \mainpage SimpleLion - C++ module
+* \section description_sec Description
+*
+* This is the documentation for SimpleLion project - C++ module. SimpleLion is a simple, multi-language localization framework inspired by Ruby on Rails localization engine. Find SimpleLion on GitHub or redmine.phitherek.mooo.com and learn more about installation, usage and other language modules.
+*
+*/
+
+#endif
